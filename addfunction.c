d596c30c9923ebff4d6cbc3cc3b2947700836a3a
sagnikishere <sagnik25adhikary@gmail.com>
2024-06-26 12:38:10 +0530

    addFunction

---
+++ addfunction.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf ("enter first number");
+    scanf("%d",&a);
+    printf("enter a second number");
+    scanf("%d",&b);
+    int d=add(a,b);
+    printf("%d",d);
+    int m=multiply(a,b);
+    printf("%d",m);
+} 
+ 
+int add(a,b)
+{
+    int s = a+b;
+	 
+    return s;
+}
+int multiply(a,b)
+{
+    int e = a*b;
+return e;
+}

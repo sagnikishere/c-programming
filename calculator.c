fd92bdf3e0ddad8227347449aa130cda0eb61234
sagnikishere <sagnik25adhikary@gmail.com>
2024-06-26 12:33:47 +0530

    basic calculator

---
+++ calculator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b,=0;
+    printf("enter two number");
+    printf("enter 1 for add\n enter 2 for substraction\n enter 3 for multiply\n 4 for divid\n");
+    scanf("%d",&ans);
+    if (ans==1)
+    printf("%d",a+b);
+    else if (ans==2)
+    printf("%d",a-b);
+    else(ans==3)
+    printf("%d",a*b);
+    else(ans==4)
+    printf("%d",a/b);
+    return 0;
+    }
